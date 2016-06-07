e0236162e09cf7c9259e96f7aa13b3f1d558d89e
Duane Winkel <duanewinkel@gmail.com>
2016-06-06 20:25:59 -0700

    added validation to existing forms

--- src/app/pages/login/login.controller.js
+++ src/app/pages/login/login.controller.js
@@ -6,13 +6,21 @@
     .controller('LoginController', LoginController);
 
   /** @ngInject */
-  function LoginController($scope) {
+  function LoginController() {
     var ctrl = this;
+
+    ctrl.submitLoginForm = submitLoginForm;
 
     activate();
 
     function activate() {
 
+    }
+
+    function submitLoginForm(form) {
+      console.log(form);
+
+      ctrl.loginError = false;
     }
   }
 })();
--- src/app/pages/signup/signup.controller.js
+++ src/app/pages/signup/signup.controller.js
@@ -6,12 +6,22 @@
     .controller('SignupController', SignupController);
 
   /** @ngInject */
-  function SignupController($scope) {
+  function SignupController() {
     var ctrl = this;
 
+    ctrl.processSignUpForm = processSignUpForm;
+    
     activate();
 
     function activate() {
+
+    }
+
+    function processSignUpForm(form) {
+      console.log(form);
+      var zipcodeLength = form.zipcode.$modelValue.toString().length;
+      console.log(zipcodeLength);
+      zipcodeLength > 5 || zipcodeLength < 5? ctrl.zipcodeError = true : ctrl.zipcodeError = false;
 
     }
   }
