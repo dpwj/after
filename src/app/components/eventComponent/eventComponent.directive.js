d0e301ab3ae555e7047c8a1a3de200459ac6a969
Duane Winkel <duanewinkel@gmail.com>
2016-06-08 20:08:38 -0700

    created the basic event component and implemented on Events page

---
+++ src/app/components/eventComponent/eventComponent.directive.js
@@ -0,0 +1,56 @@
+;(function () {
+  'use strict';
+
+  angular
+    .module('after')
+    .directive('afEventComponent', afEventComponent);
+
+  function afEventComponent() {
+    var directive = {
+      link: link,
+      restrict: 'E',
+      $scope:{},
+      templateUrl: 'app/components/eventComponent/eventComponent.html'
+
+    };
+
+    return directive;
+
+    function link($scope) {
+      var ctrl = this;
+      $scope.events = [
+        {
+         "id": "1",
+          "date": "02/02/1988",
+          "description": "this is the description",
+          "end_time": "5:00 pm",
+          "event_name": "Food Drive",
+          "start_time": "12:30 pm"
+        },
+        {
+          "id":          "1",
+          "date":        "02/02/1988",
+          "description": "this is the description",
+          "end_time":    "5:00 pm",
+          "event_name":  "Food Drive",
+          "start_time":  "12:30 pm"
+        }, {
+          "id":          "1",
+          "date":        "02/02/1988",
+          "description": "this is the description",
+          "end_time":    "5:00 pm",
+          "event_name":  "Food Drive",
+          "start_time":  "12:30 pm"
+        }, {
+          "id":          "1",
+          "date":        "02/02/1988",
+          "description": "this is the description",
+          "end_time":    "5:00 pm",
+          "event_name":  "Food Drive",
+          "start_time":  "12:30 pm"
+        }
+      ];
+      console.log(ctrl.events);
+    }
+  }
+})();
